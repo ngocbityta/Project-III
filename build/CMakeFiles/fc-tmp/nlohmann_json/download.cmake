8f2f26e4f6a5e67bf5a0bb3074f8a91be9c1b66d
Khai Tran <tranmanhkhai2004@gmail.com>
2025-10-06 20:12:46 +0700

    fix: git ignore

--- build/CMakeFiles/fc-tmp/nlohmann_json/download.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing download step for nlohmann_json")
-
-block(SCOPE_FOR VARIABLES)
-
-include("/Users/mac/Documents/Project-III/build/CMakeFiles/fc-tmp/nlohmann_json/nlohmann_json-gitclone.cmake")
-
-endblock()
